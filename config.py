6977cad51682b998a88f2046b6b3126e96c87e70
William <willc23@uci.edu>
2026-04-08 09:56:03 -0700

    chore: add global config with paths and training hyperparameters

---
+++ config.py
@@ -0,0 +1,11 @@
+DATA_PATH = "data/ratings.csv"
+MOVIES_PATH = "data/movies.csv"
+CHROMA_DIR = "chroma_db"
+CHECKPOINT_DIR = "checkpoints"
+COLLECTION_NAME = "movie_items"
+
+EMBEDDING_DIM = 64
+NUM_NEGATIVES = 4
+BATCH_SIZE = 512
+EPOCHS = 20
+LR = 1e-3
